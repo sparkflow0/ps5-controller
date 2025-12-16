f1422ca50b0f426973bc7df49dd30de166627a4f
sparkflow0 <sparkflow2030@gmail.com>
2025-12-16 10:40:13 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,18 @@ import POSPage from './pages/POS.jsx';
 
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<HomePage />} />
-    //   <Route path="/configurator" element={<ConfiguratorPage />} />
-    //   <Route path="/cart" element={<CartPage />} />
-    //   <Route path="/checkout" element={<CheckoutPage />} />
-    //   <Route path="/payment" element={<PaymentPage />} />
-    //   <Route path="/payment/confirmation" element={<ConfirmationPage />} />
-    //   <Route path="/order-summary" element={<OrderSummaryPage />} />
-    //   <Route path="/trackorder" element={<TrackOrderPage />} />
-    //   <Route path="/pos" element={<POSPage />} />
-    //   <Route path="*" element={<Navigate to="/" replace />} />
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/configurator" element={<ConfiguratorPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/payment" element={<PaymentPage />} />
+      <Route path="/payment/confirmation" element={<ConfirmationPage />} />
+      <Route path="/order-summary" element={<OrderSummaryPage />} />
+      <Route path="/trackorder" element={<TrackOrderPage />} />
+      <Route path="/pos" element={<POSPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
